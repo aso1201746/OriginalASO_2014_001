7bf80c5c376a2394efbb65e9f99605dd284eadf0
student <student@PC111S04.fgakusei.local>
2014-06-27 10:22:24 +0900

    20140627

---
+++ src/jp/ac/st/asojuku/originalaso_2014_001/DaikyouuActivity.java
@@ -0,0 +1,26 @@
+package jp.ac.st.asojuku.originalaso_2014_001;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DaikyouuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daikyouu_activity);
+		
+		// 渡ってきたintentを取得
+		Intent intent = getIntent();
+		// intentに混入していたEtraから、キーワードをもとの値に取得
+		String name = intent.getStringExtra("name");
+		
+		TextView tv = (TextView)findViewById(R.id.txvMsg);
+		tv.setText(name + "さんの今日の運命は・・・");
+		
+	}
+	
+}
